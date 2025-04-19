321828d86ef726c0f84728c21e5ebad767c9ee57
Sam Dunlap <sam.dunlap15@gmail.com>
2025-04-19 14:57:07 -0700

    fetch from real database, change name lookup to id lookup

--- app/api/teamFetchFuncs.ts
+++ app/api/teamFetchFuncs.ts
@@ -1,17 +1,39 @@
-import Team from "@/types/team"
-import { dbTeams } from "@/mockdb"
+import Team from "@/types/team";
+import sql from "./db";
+import { Err, Ok, Result } from "ts-results";
+import { DraftStatus } from "@/types";
+import { DatabaseError } from "@/types/err";
 
-export function allTeams(): Team[] {
-    const teams: Team[] = dbTeams
-    return teams
+export async function fetchTeamsByDraftID(id: string): Promise<Team[]> {
+	const res = (await sql`select * from teams where draft=${id}`) as unknown;
+	return res as Team[];
 }
 
-export function teamByTeamName(query: string): Team | undefined {
-    const teams = dbTeams;
-    return teams.find(t => t.team_name === decodeURI(query));
+export async function fetchAllTeams(): Promise<Team[]> {
+	const res = (await sql`select * from teams`) as unknown;
+	return res as Team[];
 }
 
-export function teamByCoachName(query: string): Team | undefined {
-    const teams = dbTeams;
-    return teams.find(t => t.coach === query);
+export async function fetchTeamsByCoachName(
+	name: string
+): Promise<Result<Team[], DatabaseError>> {
+	let res =
+		(await sql`select * from teams where status != ${DraftStatus.Archived} and coach_name = ${name}`) as Array<Team>;
+	if (res.length == 0) {
+		return new Err(DatabaseError.QueryReturnedNoResultsError);
+	} else {
+		return Ok(res);
+	}
+}
+
+export async function fetchTeamByID(
+	team_id: string
+): Promise<Result<Team, DatabaseError>> {
+	let res =
+		(await sql`select * from teams where uuid = ${team_id}`) as Array<Team>;
+	if (res.length == 0) {
+		return Err(DatabaseError.QueryReturnedNoResultsError);
+	} else {
+		return Ok(res[0]);
+	}
 }

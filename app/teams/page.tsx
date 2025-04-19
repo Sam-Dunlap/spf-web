e2f33cd7a37bf9a27fc9e5415eaf9b59a1c2a188
Sam Dunlap <sam.dunlap15@gmail.com>
2025-04-19 15:01:08 -0700

    updating team field names for clarity

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,13 +1,13 @@
 import {TeamPreview, TeamPreviewLoading} from "../components/teampreview";
 import {Suspense} from 'react';
-import { allTeams } from "@/api";
+import { fetchAllTeams } from "@/api";
 
 
 
-export default function TeamList() {
-    let teams = allTeams();
+export default async function TeamList() {
+    let teams = await fetchAllTeams();
     teams.sort((a, b) => (b.wins - b.losses) - (a.wins - a.losses))
-    const previews = teams.map(team => <li className="pb-2" key={team.team_name}><Suspense fallback={<TeamPreviewLoading />}><TeamPreview>{team}</TeamPreview></Suspense></li>);
+    const previews = teams.map(team => <li className="pb-2" key={team.uuid}><Suspense fallback={<TeamPreviewLoading />}><TeamPreview>{team}</TeamPreview></Suspense></li>);
     return (
             <ul className="mt-4 ml-4 col-start-1 col-span-9">
                 {previews}
